1afa0f76a15b697c603b15d10b2a4d1b736a303e
Michal Pavlíček (linux) <michaelg.pavlicek@gmail.com>
2023-10-19 13:11:00 +0200

    IZP - Projekt 2 fixed reading file and added checks

--- izp/projekt_2/bludiste.c
+++ izp/projekt_2/bludiste.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct
 {
@@ -21,7 +23,8 @@ int main()
     Map map;
     map.rows = rows;
     map.cols = cols;
-    map.cells[rows * cols + 1];
+    map.cells = malloc((cols * rows + 1) * sizeof(char));
+    map.cells[cols * rows] = '\0';
 
     char line_str[cols * 2]; // * 2 for whitespace
     int line = 0;
@@ -31,16 +34,29 @@ int main()
         fgetc(file); // Ignore \n
         printf("%s\n", line_str);
 
+        if (strlen(line_str) != cols * 2 - 1)
+        {
+            printf("The number of columns given does not correspond to the actual maze number of columns.\n");
+            return 1;
+        }
+
         for (int i = 0; i < cols; i++)
         {
-            printf("- index %d\n", line * cols + i);
-            map.cells[cols * line + i] = '\0'; // line_str[i * 2];
+            map.cells[cols * line + i] = line_str[i * 2];
         }
-        map.cells[rows * cols] = '\0';
 
         line++;
     }
 
+    if (line != map.rows)
+    {
+        printf("The number of rows given does not correspond to the actual maze number of rows.\n");
+        return 1;
+    }
+
+    printf("line: %d\n", line);
+
+    free(map.cells);
     fclose(file);
 
     return 0;
