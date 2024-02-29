4d061b2ea7fa73ba8eea7984b907ff71e5157ca1
Michal Pavlíček (linux) <michaelg.pavlicek@gmail.com>
2024-02-29 10:05:16 +0100

    INC: some basic work proj. 1

--- inc/template_rx/uart_rx.vhd
+++ inc/template_rx/uart_rx.vhd
@@ -1,5 +1,5 @@
 -- uart_rx.vhd: UART controller - receiving (RX) side
--- Author(s): Name Surname (xlogin00)
+-- Author(s): Michal Pavlíček (xpavlim00)
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -19,19 +19,75 @@ entity UART_RX is
 end entity;
 
 
-
 -- Architecture implementation (INSERT YOUR IMPLEMENTATION HERE)
 architecture behavioral of UART_RX is
+	signal clk_cnt		: std_logic_vector(4 downto 0);
+	signal bit_cnt		: std_logic_vector(2 downto 0);
+	signal activate_clk_cnt : std_logic = '0';
+	signal activate_bit_cnt : std_logic = '0';
+		
 begin
+	-- Instance of RX FSM
+	fsm: entity work.UART_RX_FSM
+	port map (
+		CLK => 		CLK,
+		RST => 		RST,
+		DIN => 		DIN,
+		CNT_CLK =>	clk_cnt,
+		CNT_BIT =>	bit_cnt,
 
-    -- Instance of RX FSM
-    fsm: entity work.UART_RX_FSM
-    port map (
-        CLK => CLK,
-        RST => RST
-    );
+		-- Activation bits 
+		ACTIVATE_CLK_CNT => activate_clk_cnt,
+		ACTIVATE_BIT_CNT => activate_bit_cnt,
+		DATA_VALID => 	    DOUT_VLD
+	);
+	
+	process (CLK) begin
+		if RST = '1' then
+			DOUT <= "00000000";
+			DOUT_VLD <= '0';
+			clk_cnt <= "00000";
+			bit_cnt <= "000";
+		elsif rising_edge(CLK)
+			if activate_clk_cnt = '1' then
+				clk_cnt <= clk_cnt + 1;
+			else 
+				clk_cnt <= "00000";
+			end if;
 
-    DOUT <= (others => '0');
-    DOUT_VLD <= '0';
+			if activate_bit_cnt = '1' then
+				if clk_cnt >= "10000" then
+					clk_cnt <= "00000";
+
+					case bit_cnt is
+						when "000" =>
+							bit_cnt <= "001";
+							DOUT[0] <= DIN;
+						when "001" =>
+							bit_cnt <= "010";
+							DOUT[1] <= DIN;
+						when "010" =>
+							bit_cnt <= "011";
+							DOUT[2] <= DIN;
+						when "011" =>
+							bit_cnt <= "100";
+							DOUT[3] <= DIN;
+						when "100" =>
+							bit_cnt <= "101";
+							DOUT[4] <= DIN;
+						when "101" =>
+							bit_cnt <= "110";
+							DOUT[5] <= DIN;
+						when "110" =>
+							bit_cnt <= "111";
+							DOUT[6] <= DIN;
+						when "111" =>
+							bit_cnt <= "000";
+							DOUT[7] <= DIN;
+					end case;
+				end if;
+			end if;
+		end if;
+	end process;
 
 end architecture;
--- inc/template_rx/uart_rx_fsm.vhd
+++ inc/template_rx/uart_rx_fsm.vhd
@@ -1,5 +1,5 @@
 -- uart_rx_fsm.vhd: UART controller - finite state machine controlling RX side
--- Author(s): Name Surname (xlogin00)
+-- Author(s): Michal Pavlíček  (xpavlim00)
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -8,15 +8,57 @@ use ieee.std_logic_unsigned.all;
 
 
 entity UART_RX_FSM is
-    port(
-       CLK : in std_logic;
-       RST : in std_logic
-    );
+	port(
+		CLK     : in std_logic;
+		RST     : in std_logic;
+		DIN     : in std_logic;
+		CNT_CLK : in std_logic_vector(4 downto 0); -- keeps track of how many clock cycles happened
+		CNT_BIT : in std_logic_vector(2 downto 0); -- tracks how many bits were received
+		
+		-- Activation bits
+		ACTIVATE_CLK_CNT	: in std_logic;
+		DATA_VALID		: in std_logic;
+	);
 end entity;
 
 
-
 architecture behavioral of UART_RX_FSM is
+type FSM_STATES is (
+	Idle,
+	Start,
+	Data,
+	Stop,
+	Valid
+)
+signal curr_state : FSM_STATES := Idle;
 begin
-
+	DATA_VALID <= '1' when curr_state = Valid else '0';
+	ACTIVATE_CLK_CNT <= '0' when curr_state = Idle or curr_state = Valid else '1';
+process (CLK) begin
+	if rising_edge(CLK) then
+		if RST = '1' then
+			curr_state <= Idle
+		else
+			case curr_state is
+				when Idle =>
+					if DIN = '0' then
+						curr_state <= Start;
+					end if;
+				when Start =>
+					if CNT_CLK = "11000" then -- 24
+						state <= Data;
+					end if;
+				when Data =>
+					if CNT_BIT = "111" then -- 7
+						state <= Stop;
+					end if;
+				when Stop =>
+					if DIN = '1' then
+						curr_state <= Valid;
+					end if;
+				when Valid =>
+					curr_state <= Idle;
+			end case;
+		end if;
+	end if;
 end architecture;
